2bb801547bf7da3f82aed1ad15be1166ff2a6c74
Jasiek <okraskaj@ee.pw.edu.pl>
2016-06-16 02:57:40 +0200

    >logging and registering now works with database >adding posts working >post view working

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask,request,render_template, make_response
+from flask import Flask, request, render_template, make_response, redirect
 from pymongo import MongoClient
-import hashlib
 import uuid
+from pass_cor import hash_password, check_password, calculate_entropy
 
 app = Flask(__name__)
-
-
 
 client = MongoClient("mongodb://localhost")
 db = client.facebuk
@@ -15,50 +13,14 @@ usersdb = db.users
 users = {}
 cursor = db.users.find()
 for document in cursor:
-
-    #passwdhash = document['passwdhash']
-    temp_user ={}
-    temp_user["id"]=document['_id']
-    temp_user["login"]=document['login']
+    temp_user = {}
+    temp_user["id"] = document['_id']
+    temp_user["login"] = document['login']
     temp_user["hashpasswd"] = document['hashpasswd']
-    temp_user["name"] = document['name']
-    temp_user["height"] = document['height']
-    temp_user["age"] = document['age']
-    users[document['login']]=temp_user
+    users[document['login']] = temp_user
 
-def hash_password(password):
-    salt = uuid.uuid4().hex
-    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
-
-def check_password(hashed_password, given_password):
-    password, salt = hashed_password.split(':')
-    return password == hashlib.sha256(salt.encode() + given_password.encode()).hexdigest()
-
-def calculate_entropy(password=''):
-    import re
-    from math import log, pow
-    numeric = re.compile('\d')
-    loweralpha = re.compile('[a-z]')
-    upperalpha = re.compile('[A-Z]')
-    symbols = re.compile('[-_.:,;<>?"#$%&/()!@~]')
-    polish = re.compile('[ąęźćśóżńĄĘŻŹŚĆŃÓ]')
-    num_of_symbols = 20
-    charset = 0
-    if numeric.search(password):
-        charset += 10
-    if loweralpha.search(password):
-        charset += 26
-    if upperalpha.search(password):
-        charset += 26
-    if polish.search(password):
-        charset += 16
-    if symbols.search(password):
-        charset += num_of_symbols
-
-    entropy = log(pow(charset, len(password)), 2)
-    return entropy
-
-
+def is_login_in_db(login):
+    return db.users.find_one({'login': login})
 
 
 @app.route('/')
@@ -67,76 +29,121 @@ def index(user=None):
     return render_template("profile.html", user=user)
 
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login(login=None):
     if request.method == "POST":
         login = request.form['login']
         passwd = request.form['passwd']
         resp = make_response(render_template('login.html', messg="Succesfully logged in!"))
 
-        if login in users.keys():
+        if is_login_in_db(login):
             real_password = users[login]['hashpasswd']
             if check_password(real_password, passwd):
-                cookie = uuid.uuid4().hex
-                resp.set_cookie('logged',cookie)
-                users[login]['cookie']= cookie
+                cookie = str(uuid.uuid4().hex)+":"+login
+                users[login]['cookie'] = cookie
+                resp.set_cookie('logged', cookie)
                 return resp
             else:
-                return render_template('login.html', error="Bad password")
-
+                return render_template('login.html', error="Bad password or login")
         else:
-            return render_template('login.html', error="Bad login")
+            return render_template('login.html', error="Bad login or password")
 
 
     elif request.method == "GET":
         return render_template("login.html")
 
 
-@app.route('/register',methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register(user=None):
     if request.method == "POST":
         login = request.form['login']
-        passwd = request.form['passwd']
-        passwdrep = request.form['passwdrep']
-        if login in users:
-            return render_template('register.html',error="This login is alredy taken")
+        passwd = request.form['passwd'].strip()
+        if is_login_in_db(login):
+            return render_template('register.html',
+                                   error="This login is alredy taken")
         else:
-            if passwdrep == passwd:
-                if len(passwd)<5:
-                    return render_template('register.html',error="Password have to contain at least 5 characters")
+            if len(passwd) < 5:
+                return render_template('register.html',
+                                       error="Password have to contain at least 5 characters")
+            else:
+                entropy = calculate_entropy(passwd)
+                if entropy < 47:
+                    return render_template('register.html',
+                                           error="You have to create harder password. Consider using upper case letters and digits. %d" % entropy)
                 else:
-                    entropy = calculate_entropy(passwd)
-                    if entropy<47:
-                        return render_template('register.html', error="You have to create harder password. Consider using upper case letters and digits. %d" % entropy)
-                    else:
-                        #now we know that everything is ok
-                        new_user = {}
-                        new_user['login']= login
-                        new_user['hashpasswd']= hash_password(passwd)
-                        users[login]=new_user
-                        return render_template('login.html',messg="You've registered succesfully. You can now log in.")
-
-
+                    # now we know that everything is ok
+                    new_user = {}
+                    new_user['login'] = login
+                    hash = hash_password(passwd)
+                    new_user['hashpasswd'] = hash
+                    _id = db.users.insert(
+                        {
+                            "login": login,
+                            "hashpasswd": hash
+                        }
+                    )
+                    new_user['id'] = _id
+                    users[login] = new_user
+                    return render_template('register.html',
+                                           messg="You've registered succesfully. You can now log in.",
+                                           registered="True" )
 
     elif request.method == "GET":
         return render_template("register.html")
 
 
-@app.route('/posttest',methods=['GET','POST'])
-def posttest():
+
+@app.route('/posts')
+def posts(user=None):
+    posts_raw = db.posts.find()
+    posts = []
+    for document in posts_raw:
+        new_post = {}
+        new_post['title']=document['title']
+        new_post['content']=document['content']
+        new_post['author_login']=document['author']
+        new_post['id']=document['_id']
+        posts.append(new_post)
+
+    return render_template('globalwall.html',posts = posts)
+
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+def postnew(user=None):
+    cookie = request.cookies.get('logged')
+    local_login = cookie.split(':')[1]
+
     if request.method == "POST":
-        return "POST HAS BEEN USED"
-    else:
-        return 'Method used: %s' % request.method
+        if cookie and users[local_login]['cookie'] == cookie:
+            title = request.form['title']
+            content = request.form['content'].strip()
+            author = local_login
+            _id = db.posts.insert(
+                {
+                    "title": title,
+                    "content": content,
+                    "author": author
+                }
+            )
+            return redirect('/posts')
 
-@app.route('/profile/<login>')
-def profile(login):
-    if login in users:
-        return render_template("profile.html",user=users[login])
+    elif request.method == "GET":
+        posts_raw = db.posts.find()
+        posts = []
+        if cookie and users[local_login]['cookie'] == cookie:
+            for document in posts_raw:
+                new_post = {}
+                new_post['title']=document['title']
+                new_post['content']=document['content']
+                new_post['author']=document['author']
+                new_post['id']=document['_id']
+                posts.append(new_post)
 
-@app.route('/userslist')
-def users_list():
-    return render_template("users.html", users=users)
+            return render_template('newpost.html',posts = posts)
+        else:
+            return redirect('/login',error="To add new posts you have to be logged in")
+
 
 #
 # @app.route('/profile/<username>/post/<int::post_id>')
@@ -147,8 +154,6 @@ def users_list():
 #             return 'This is the %s post numer %i' % username % post_id
 #         else:
 #             return 'This user is not created yet'
-
-
 
 
 if __name__ == '__main__':
---
+++ pass_cor.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import hashlib
+import uuid
+
+
+def hash_password(password):
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+def check_password(hashed_password, given_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + given_password.encode()).hexdigest()
+
+def calculate_entropy(password=''):
+    import re
+    from math import log, pow
+    numeric = re.compile('\d')
+    loweralpha = re.compile('[a-z]')
+    upperalpha = re.compile('[A-Z]')
+    symbols = re.compile('[-_.:,;<>?"#$%&/()!@~]')
+    polish = re.compile('[ąęźćśóżńĄĘŻŹŚĆŃÓ]')
+    num_of_symbols = 20
+    charset = 0
+    if numeric.search(password):
+        charset += 10
+    if loweralpha.search(password):
+        charset += 26
+    if upperalpha.search(password):
+        charset += 26
+    if polish.search(password):
+        charset += 16
+    if symbols.search(password):
+        charset += num_of_symbols
+
+    entropy = log(pow(charset, len(password)), 2)
+    return entropy
+
+
